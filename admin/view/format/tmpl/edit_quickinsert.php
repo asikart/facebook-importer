c64183c1f969caaa8dac5dde7733aa9a1161d888
Simon Asika <asika32764@gmail.com>
2015-03-12 00:40:57 +0800

    Fix format edit quickset bug

--- admin/view/format/tmpl/edit_quickinsert.php
+++ admin/view/format/tmpl/edit_quickinsert.php
@@ -33,70 +33,70 @@
 		<p><?php echo JText::_('COM_FBIMPORTER_CLICK_AND_INSERT'); ?></p>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{TITLE}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {TITLE}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{TITLE}', 'jform_text')"><i class="icon-arrow-left-3"></i> {TITLE}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_ARTICLE_TITLE'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{VIDEO}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {VIDEO}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{VIDEO}', 'jform_text')"><i class="icon-arrow-left-3"></i> {VIDEO}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_VIDEO_NEED_ALLVIDEOS'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{INTRO_MESSAGE}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {INTRO_MESSAGE}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{INTRO_MESSAGE}', 'jform_text')"><i class="icon-arrow-left-3"></i> {INTRO_MESSAGE}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_ARTICLE_INTROTEXT'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{FULL_MESSAGE}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {FULL_MESSAGE}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{FULL_MESSAGE}', 'jform_text')"><i class="icon-arrow-left-3"></i> {FULL_MESSAGE}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_ARTICLE_CONTENT'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{IMAGE}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {IMAGE}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{IMAGE}', 'jform_text')"><i class="icon-arrow-left-3"></i> {IMAGE}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_IMAGE'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{READMORE_LINK}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {READMORE_LINK}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{READMORE_LINK}', 'jform_text')"><i class="icon-arrow-left-3"></i> {READMORE_LINK}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_FB_ORIGIN_LINK'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{LINK_URL}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {LINK_URL}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{LINK_URL}', 'jform_text')"><i class="icon-arrow-left-3"></i> {LINK_URL}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_LINK_URL'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{LINK_NAME}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {LINK_NAME}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{LINK_NAME}', 'jform_text')"><i class="icon-arrow-left-3"></i> {LINK_NAME}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_LINK_TITLE'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{LIKES}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {LIKES}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{LIKES}', 'jform_text')"><i class="icon-arrow-left-3"></i> {LIKES}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_FB_LIKE_COUNT'); ?></span>
 			</div>
 		</div>
 
 		<div class="control-group">
-			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{CREATED_TIME}', 'jform_basic_text')"><i class="icon-arrow-left-3"></i> {CREATED_TIME}</button>
+			<button type="button" class="btn btn-small" style="cursor: pointer;" onclick="jInsertEditorText('{CREATED_TIME}', 'jform_text')"><i class="icon-arrow-left-3"></i> {CREATED_TIME}</button>
 			<div class="controls">
 				<span><?php echo JText::_('COM_FBIMPORTER_FB_PUBLISH_UP_TIME'); ?></span>
 			</div>
